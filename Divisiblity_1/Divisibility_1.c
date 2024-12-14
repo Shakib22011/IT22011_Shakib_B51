56a0f833d9551b2be1818cd20f937cd668e6507a
Shakib22011 <142972771+Shakib22011@users.noreply.github.com>
2024-12-14 09:23:29 +0600

    Add files via upload

---
+++ Divisiblity_1/Divisibility_1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n+1];
+    for(int i=1;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    if(a[n]%10==0)
+        printf("Yes\n");
+    else
+        printf("No\n");
+}
